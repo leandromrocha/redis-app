a72beb993108c8ce927346c2cc73019ba6537fcf
Leandro Monteiro Rocha <leandro.mrocha@gmail.com>
2023-07-11 16:04:19 -0300

    Ajuste no Jenkinsfile

--- teste-app.sh
+++ teste-app.sh
@@ -1,23 +1,25 @@
 pipeline {
     agent any
-    stage('build da imagem docker'){
-        steps{
-            sh 'docker build -t devops/app .'
+    stages {
+        stage('build da imagem docker'){
+            steps{
+                sh 'docker build -t devops/app .'
+            }
         }
-    }
     stage('subir docker compose - redis e app'){
-        steps {
-            sh 'docker-compose up --build -d'
+            steps {
+                sh 'docker-compose up --build -d'
+            }
         }
-    }
     stage('sleep para subida de containers'){
-        steps{
-            sh 'sleep 10'
+            steps{
+                sh 'sleep 10'
+            }
         }
-    }
     stage('teste da aplicação'){
-        steps{
+            steps{
             sh 'teste.sh'
+            }
         }
     }
 }
