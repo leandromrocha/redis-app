c41da9573f5e36ec916dc7a8d444b08e4a7630e0
Leandro Monteiro Rocha <leandro.mrocha@gmail.com>
2023-07-12 10:13:18 -0300

    Ajustes no arquivos teste-app

---
+++ teste-app.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+RESULT="'wget -qO- http://localhost:8090'"
+wget -q localhost:8090
+if [$? -eq 0]
+then
+    echo 'Ok - serviço no ar!'
+elif [[ $RESULT == *"Number"* ]]
+then
+    echo 'Ok - number of visits'
+    echo $RESULT
+else
+    echo 'Not Ok - number of visits'
+    exit 1
+fi
